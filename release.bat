f965b0a7b41b1f47860e7ebecc7427fdfd256866
0xEAB <desisma@heidel.beer>
2018-07-05 23:09:11 +0200

    Improve windows build script
    Get rid of locale-dependent commands
    Change build dir and make it relative to %windir%
    Sync build dir instead of delete+copy
    Move output zip instead of copying it

--- release.bat
+++ release.bat
@@ -1,19 +1,32 @@
 rem Building & compressing serve-d for release inside a virtual machine with Windows 8 or above
-rem This will delete the folder C:\build
 
-del /S /Q C:\build
-xcopy /e . C:\build
-pushd C:\build
+cd %~dp0
+
+@if not exist version.txt (
+	echo.
+    echo !-- Error: version.txt is missing :/
+    echo.
+    pause
+    goto :eof
+)
+
+rem This will sync this repo with the folder %windir%\..\buildsd
+robocopy . %windir%\..\buildsd /MIR /XA:SH /XD .* /XF .* /XF *.zip
+pushd %windir%\..\buildsd
+
 set /p Version=<version.txt
 dub build --compiler=ldc2 --arch=x86
-echo Y | del windows
+
+if exist windows del /S /Q windows
 mkdir windows
-echo F | xcopy /f serve-d.exe windows\serve-d.exe
-echo F | xcopy /f libcurl.dll windows\libcurl.dll
-echo F | xcopy /f libeay32.dll windows\libeay32.dll
-echo F | xcopy /f ssleay32.dll windows\ssleay32.dll
-del C:\build\windows.zip
+copy serve-d.exe windows\serve-d.exe
+copy libcurl.dll windows\libcurl.dll
+copy libeay32.dll windows\libeay32.dll
+copy ssleay32.dll windows\ssleay32.dll
+
+if exist windows.zip del windows.zip
 powershell -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::CreateFromDirectory('windows', 'windows.zip'); }"
 popd
-echo F | xcopy C:\build\windows.zip serve-d-%Version%-windows.zip
+
+move %windir%\..\buildsd\windows.zip "serve-d-%Version%-windows.zip"
 pause
