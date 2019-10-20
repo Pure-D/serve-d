7558afedd87c556a703c5ba7b16527aec7c6d950
WebFreak001 <gh@webfreak.org>
2019-10-20 20:34:45 +0200

    allow for beta versions in version output

--- source/app.d
+++ source/app.d
@@ -166,7 +166,7 @@ void printVersion(io.File output = io.stdout)
 		import source.workspaced.info : BundledDependencies, WorkspacedVersion = Version;
 	import source.served.info;
 
-	output.writefln("serve-d v%(%s.%) with workspace-d v%(%s.%)", Version, WorkspacedVersion);
+	output.writefln("serve-d v%(%s.%)%s with workspace-d v%(%s.%)", Version, VersionSuffix.length ? text('-', VersionSuffix) : VersionSuffix, WorkspacedVersion);
 	output.writefln("Included features: %(%s, %)", IncludedFeatures);
 	// There will always be a line which starts with `Built: ` forever, it is considered stable. If there is no line, assume version 0.1.2
 	output.writefln("Built: %s", __TIMESTAMP__);
--- source/served/info.d
+++ source/served/info.d
@@ -1,3 +1,4 @@
 module source.served.info;
 
 static immutable Version = [0, 4, 1];
+static immutable VersionSuffix = ""; // like beta.1
