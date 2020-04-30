4d16962aa478bbdf5d697ee7aac2563931425d68
WebFreak001 <gh@webfreak.org>
2020-05-01 00:18:44 +0200

    separate out range/severity generator for dscanner
    Also fixes that errors were previously always reported as warnings

--- source/served/linters/dscanner.d
+++ source/served/linters/dscanner.d
@@ -43,31 +43,18 @@ void lint(Document document)
 		if (ignoredKeys.canFind(issue.key))
 			continue;
 		Diagnostic d;
-		auto s = issue.description;
 		scope text = document.lineAtScope(cast(uint) issue.line - 1).stripRight;
 		string keyNormalized = issue.key.startsWith("dscanner.")
 			? issue.key["dscanner.".length .. $] : issue.key;
-		if (text.canFind("@suppress(all)", "@suppress:all", "@suppress(" ~ issue.key ~ ")",
-				"@suppress:" ~ issue.key, "@suppress(" ~ keyNormalized ~ ")", "@suppress:" ~ keyNormalized)
-				|| text.endsWith("stfu"))
+		if (text.canFind("@suppress(all)", "@suppress:all",
+				"@suppress(" ~ issue.key ~ ")", "@suppress:" ~ issue.key,
+				"@suppress(" ~ keyNormalized ~ ")", "@suppress:" ~ keyNormalized) || text
+				.endsWith("stfu"))
 			continue;
-		if (s.startsWith("Line is longer than ") && s.endsWith(" characters"))
-			d.range = TextRange(Position(cast(uint) issue.line - 1,
-					s["Line is longer than ".length .. $ - " characters".length].to!uint),
-					Position(cast(uint) issue.line - 1, 1000));
-		else
-			d.range = document.wordRangeAt(Position(cast(uint) issue.line - 1, cast(uint) issue.column - 1));
 
-		if (issue.key == "dscanner.suspicious.unused_parameter"
-				|| issue.key == "dscanner.suspicious.unused_variable")
-		{
-			d.severity = DiagnosticSeverity.hint;
-			d.tags = opt([DiagnosticTag.unnecessary]);
-		}
-		else
-		{
-			d.severity = issue.type ? DiagnosticSeverity.warning : DiagnosticSeverity.error;
-		}
+		d.adjustRangeForType(document, issue);
+		d.adjustSeverityForType(document, issue);
+
 		d.source = DScannerDiagnosticSource;
 		d.message = issue.description;
 		d.code = JSONValue(issue.key);
@@ -96,4 +83,51 @@ string getDscannerIniForDocument(DocumentUri document, WorkspaceD.Instance insta
 	if (!exists(ini))
 		ini = "dscanner.ini";
 	return ini;
+}
+
+void adjustRangeForType(ref Diagnostic d, Document document, DScannerIssue issue)
+{
+	switch (issue.key)
+	{
+	case null:
+		// syntax errors
+		adjustRangeForSyntaxError(d, document, issue);
+		break;
+	default:
+		d.range = document.wordRangeAt(Position(cast(uint) issue.line - 1,
+				cast(uint) issue.column - 1));
+		break;
+	}
+}
+
+private void adjustRangeForSyntaxError(ref Diagnostic d, Document document, DScannerIssue issue)
+{
+	auto s = issue.description;
+
+	if (s.startsWith("Line is longer than ") && s.endsWith(" characters"))
+	{
+		d.range = TextRange(Position(cast(uint) issue.line - 1,
+				s["Line is longer than ".length .. $ - " characters".length].to!uint),
+				Position(cast(uint) issue.line - 1, 1000));
+	}
+	else
+	{
+		d.range = document.wordRangeAt(Position(cast(uint) issue.line - 1,
+				cast(uint) issue.column - 1));
+	}
+}
+
+void adjustSeverityForType(ref Diagnostic d, Document, DScannerIssue issue)
+{
+	if (issue.key == "dscanner.suspicious.unused_parameter"
+			|| issue.key == "dscanner.suspicious.unused_variable")
+	{
+		d.severity = DiagnosticSeverity.hint;
+		d.tags = opt([DiagnosticTag.unnecessary]);
+	}
+	else
+	{
+		d.severity = issue.type == "error" ? DiagnosticSeverity.error : DiagnosticSeverity
+			.warning;
+	}
 }
