ec028212c64d10ea04a2c34d19dcdfd50c3b05fa
Jan Jurzitza <gh@webfreak.org>
2021-03-23 17:32:34 +0000

    fix formatting

--- source/served/commands/test_provider.d
+++ source/served/commands/test_provider.d
@@ -139,7 +139,7 @@ private void rescanProject(WorkspaceD.Instance instance)
 {
 	import std.datetime.stopwatch : StopWatch;
 	import std.path : buildNormalizedPath;
-	import std.algorithm.searching: endsWith;
+	import std.algorithm.searching : endsWith;
 
 	DubComponent dub = instance.get!DubComponent;
 	auto settings = dub.rootPackageBuildSettings();
@@ -159,7 +159,7 @@ private void rescanProject(WorkspaceD.Instance instance)
 		auto fullPath = buildNormalizedPath(settings.packagePath, path);
 		auto uri = fullPath.uriFromFile;
 
-		if(!uri.endsWith(".d") && !uri.endsWith(".dpp") && !uri.endsWith(".di")) continue;
+		if (!uri.endsWith(".d") && !uri.endsWith(".dpp") && !uri.endsWith(".di")) continue;
 
 		bool tempLoad = documents.tryGet(uri) == Document.init;
 		if (tempLoad)
