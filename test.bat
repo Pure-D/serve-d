d1e03762fc332094fba4a1a01c9aba195488183a
WebFreak001 <gh@webfreak.org>
2021-04-10 14:03:23 +0200

    add scripts for local testing

---
+++ test.bat
@@ -0,0 +1,1 @@
+dub test :http && dub test :lsp && dub test :serverbase && dub test
---
+++ test.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+set -euo pipefail
+# hehe, this is the best kind of windows emulation
+# test.bat contains just a list of dub commands, so we don't have to maintain this file twice.
+. ./test.bat
