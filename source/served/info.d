f191f1baefa9693c3840d8597a807ed0c2b98954
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-05-25 04:48:52 -0500

    bump to 0.7.0-beta.6 (#136)

--- source/served/info.d
+++ source/served/info.d
@@ -1,4 +1,4 @@
 module source.served.info;
 
 static immutable Version = [0, 7, 0];
-static immutable VersionSuffix = "beta.5"; // like beta.1
+static immutable VersionSuffix = "beta.6"; // like beta.1
