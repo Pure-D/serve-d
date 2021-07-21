c10af2c77e4b4a110c4fe2efcf470314ce878246
WebFreak001 <gh@webfreak.org>
2021-07-22 00:13:14 +0200

    add uri utilities: dirName, build, normalize

--- lsp/source/served/lsp/uri.d
+++ lsp/source/served/lsp/uri.d
@@ -1,6 +1,8 @@
 module served.lsp.uri;
 
 import served.lsp.protocol;
+import std.algorithm;
+import std.conv;
 import std.path;
 import std.string;
 
@@ -77,4 +79,83 @@ DocumentUri uri(string scheme, string authority, string path, string query, stri
 		~ (path.length ? path : "/")
 		~ (query.length ? "?" ~ query : "")
 		~ (fragment.length ? "#" ~ fragment : "");
+}
+
+DocumentUri uriDirName(DocumentUri uri)
+{
+	auto slash = uri.lastIndexOf('/');
+	if (slash <= 0)
+		return uri;
+	if (uri[slash - 1] == '/')
+	{
+		if (slash == uri.length - 1)
+			return uri;
+		else
+			return uri[0 .. slash + 1];
+	}
+	return uri[0 .. slash];
+}
+
+///
+unittest
+{
+	assert("file:///foo/bar/".uriDirName == "file:///foo/bar");
+	assert("file:///foo/bar".uriDirName == "file:///foo");
+	assert("file:///foo/bar".uriDirName.uriDirName == "file:///");
+}
+
+/// Appends the path to the uri, potentially replacing the whole thing if the
+/// path is absolute. Cleans `./` and `../` sequences using `uriNormalize`.
+DocumentUri uriBuildNormalized(DocumentUri uri, scope const(char)[] path)
+{
+	path = path.replace("\\", "/");
+
+	if (path.startsWith("/"))
+	{
+		auto scheme = uri.indexOf("://");
+		if (scheme == -1)
+			return path.idup;
+		else
+			return text(uri[0 .. scheme + 3], path[1 .. $]);
+	}
+
+	while (path.startsWith("../"))
+	{
+		uri = uri.uriDirName;
+		path = path[3 .. $];
+	}
+
+	if (path.startsWith("/"))
+		path = path[1 .. $];
+
+	return text(uri, "/", path).uriNormalize;
+}
+
+///
+unittest
+{
+	assert(uriBuildNormalized("file:///foo/bar", "baz") == "file:///foo/bar/baz");
+	assert(uriBuildNormalized("file:///foo/bar", "../baz") == "file:///foo/baz");
+	assert(uriBuildNormalized("file:///foo/bar", "/home/baz") == "file:///home/baz");
+}
+
+/// Cleans `./` and `../` from the URI
+DocumentUri uriNormalize(DocumentUri uri)
+{
+	while (true)
+	{
+		auto sameDir = uri.indexOf("./");
+		if (sameDir == -1)
+			break;
+		if (sameDir == 0)
+			uri = uri[2 .. $];
+		else if (sameDir >= 2 && uri[sameDir - 1] == '.' && uri[sameDir - 2] == '/') // /../
+			uri = uri[0 .. sameDir - 1].uriDirName ~ uri[sameDir + 2 .. $];
+		else if (uri[sameDir - 1] == '/') // /./
+			uri = uri[0 .. sameDir] ~ uri[sameDir + 2 .. $];
+		else
+			break; // might break on `a./b` here, but better than infinite loop for malformed url
+	}
+
+	return uri;
 }
