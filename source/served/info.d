e10bf4bb6a6162f8015ca6f1123133bf90817c10
WebFreak001 <gh@webfreak.org>
2022-11-04 19:33:55 +0100

    first beta release

--- source/served/info.d
+++ source/served/info.d
@@ -1,7 +1,7 @@
 module served.info;
 
-static immutable Version = [0, 7, 4];
-static immutable VersionSuffix = ""; // like beta.1
+static immutable Version = [0, 8, 0];
+static immutable VersionSuffix = "beta.1"; // like beta.1
 static immutable string BundledDependencies = "dub, dfmt and dscanner are bundled within (compiled in)";
 
 version (Windows) version (DigitalMars) static assert(false,
