dfd3fd1d954726c2a991946cc16b68d3fb2acb1c
WebFreak001 <gh@webfreak.org>
2023-01-21 00:33:27 +0100

    don't try to run cross-compiled nightlies

--- ci/build.sh
+++ ci/build.sh
@@ -3,6 +3,10 @@ set -x
 
 dub build --compiler=ldc2 --build=$BUILD --arch=$ARCH
 strip serve-d
-./serve-d --version
+if [ "$CROSS" = 1 ]; then
+	ls serve-d
+else
+	./serve-d --version
+fi
 
 tar cfJ serve-d.tar.xz serve-d
