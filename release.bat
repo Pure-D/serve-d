c283c2e6c7fc92ddf3dec01a2704e2cdc3a678ea
ryuukk <44361234+ryuukk@users.noreply.github.com>
2023-08-10 23:26:34 +0200

    Update binary path

--- release.bat
+++ release.bat
@@ -18,15 +18,8 @@ pushd %SystemDrive%\buildsd
 set /p Version=<version.txt
 dub build --compiler=ldc2 --arch=x86
 
-if exist windows del /S /Q windows
-mkdir windows
-copy serve-d.exe windows\serve-d.exe
-copy libcurl.dll windows\libcurl.dll
-copy libeay32.dll windows\libeay32.dll
-copy ssleay32.dll windows\ssleay32.dll
-
 if exist windows.zip del windows.zip
-powershell -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::CreateFromDirectory('windows', 'windows.zip'); }"
+powershell -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::CreateFromDirectory('bin', 'windows.zip'); }"
 popd
 
 move %SystemDrive%\buildsd\windows.zip "serve-d_%Version%-windows.zip"
