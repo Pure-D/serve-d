16a795e4496529bdc443ee191d4448e81b1949cd
Remi Thebault <remi.thebault@gmail.com>
2024-02-13 16:41:39 +0100

    implement CCDB based DCD imports and linting
    two new settings are added:
     - d.ccdbPath:          user specified path to `compile_commands.json` to consider as input
     - d.enableCcdbLinting: whether to activate linting of CCDB files
    
    Linting works by actually compiling the file using command specified in
    CCDB and scanning for compiler error.

--- workspace-d/source/workspaced/com/ccdb.d
+++ workspace-d/source/workspaced/com/ccdb.d
@@ -4,14 +4,15 @@
 module workspaced.com.ccdb;
 
 import std.exception;
-import std.experimental.logger : trace;
-import std.file;
 import std.json;
-import std.stdio;
+import std.path;
+import fs = std.file;
 
 import workspaced.api;
+import workspaced.com.dcd;
 
 import containers.hashset;
+import workspaced.com.dub;
 
 @component("ccdb")
 class ClangCompilationDatabaseComponent : ComponentWrapper
@@ -21,6 +22,9 @@ class ClangCompilationDatabaseComponent : ComponentWrapper
 	protected void load()
 	{
 		trace("loading ccdb component");
+
+		if (!refInstance)
+			throw new Exception("ccdb requires to be instanced");
 
 		if (config.get!bool("ccdb", "registerImportProvider", true))
 			importPathProvider = &imports;
@@ -33,35 +37,36 @@ class ClangCompilationDatabaseComponent : ComponentWrapper
 		if (config.get!bool("ccdb", "registerDebugSpecificationsProvider", true))
 			debugSpecificationsProvider = &debugVersions;
 
-		try
-		{
-			if (config.get!string("ccdb", null))
-			{
-				const dbPath = config.get!string("ccdb", "dbPath");
-				if (!dbPath)
-				{
-					throw new Exception("ccdb.dbPath is not provided");
-				}
-				loadDb(dbPath);
-			}
-		}
-		catch (Exception e)
-		{
-			stderr.writeln("Clang-DB Error (ignored): ", e);
-		}
+		if (auto dbPath = config.get!string("ccdb", "dbPath", null))
+			loadDb(dbPath);
 	}
 
-	private void loadDb(string filename)
+	void setDbPath(string dbPath)
+	{
+		import std.path : buildNormalizedPath;
+
+		if (dbPath.length)
+			loadDb(dbPath);
+		else
+			unloadDb();
+
+		config.set("ccdb", "dbPath", dbPath.buildNormalizedPath());
+
+		if (refInstance.has!DCDComponent)
+			refInstance.get!DCDComponent.refreshImports();
+	}
+
+	string getDbPath() const
+	{
+		return config.get!string("ccdb", "dbPath", null);
+	}
+
+	private void loadDb(string dbPath)
 	{
 		import std.algorithm : each, filter, map;
 		import std.array : array;
 
-		string jsonString = cast(string) assumeUnique(read(filename));
-		auto json = parseJSON(jsonString);
-		// clang db can be quite large (e.g. 100 k lines of JSON data on large projects)
-		// we release memory when possible to avoid having at the same time more than
-		// two represention of the same data
-		jsonString = null;
+		trace("parsing CCDB from ", dbPath);
 
 		HashSet!string imports;
 		HashSet!string stringImports;
@@ -69,18 +74,42 @@ class ClangCompilationDatabaseComponent : ComponentWrapper
 		HashSet!string versions;
 		HashSet!string debugVersions;
 
-		json.array
-			.map!(jv => CompileCommand.fromJson(jv))
-			.filter!(cc => cc.isValid)
-			.each!(cc =>
-					cc.feedOptions(imports, stringImports, fileImports, versions, debugVersions)
-			);
+		_compileCommands.clear();
+
+		{
+			string jsonString = cast(string) assumeUnique(fs.read(dbPath));
+			auto json = parseJSON(jsonString);
+			// clang db can be quite large (e.g. 100 k lines of JSON data on large projects)
+			// we release memory when possible to avoid having at the same time more than
+			// two represention of the same data
+			jsonString = null;
+
+			auto ccRng = json.array
+				.map!(jv => CompileCommand.fromJson(jv))
+				.filter!(cc => cc.isValid);
+
+			foreach (cc; ccRng)
+			{
+				cc.feedOptions(imports, stringImports, fileImports, versions, debugVersions);
+				_compileCommands[cc.getNormalizedFilePath()] = cc;
+			}
+		}
 
 		_importPaths = imports[].array;
 		_stringImportPaths = stringImports[].array;
 		_importFiles = fileImports[].array;
 		_versions = versions[].array;
 		_debugVersions = debugVersions[].array;
+	}
+
+	private void unloadDb()
+	{
+		_importPaths = null;
+		_stringImportPaths = null;
+		_importFiles = null;
+		_versions = null;
+		_debugVersions = null;
+		_compileCommands.clear();
 	}
 
 	/// Lists all import paths
@@ -113,27 +142,41 @@ class ClangCompilationDatabaseComponent : ComponentWrapper
 		return _debugVersions;
 	}
 
+	/// Return the compile command for the given D source file, or null if this file is not
+	/// in the database.
+	CompileCommand getCompileCommand(string filename) @property
+	{
+		auto normalized = buildNormalizedPath(filename);
+		auto ccp = normalized in _compileCommands;
+		if (ccp)
+			return ccp.dup;
+		return CompileCommand.init;
+	}
+
 private:
 
 	string[] _importPaths, _stringImportPaths, _importFiles, _versions, _debugVersions;
+	CompileCommand[string] _compileCommands;
 }
 
-private struct CompileCommand
+public struct CompileCommand
 {
 	string directory;
 	string file;
 	string[] args;
 	string output;
 
-	static CompileCommand fromJson(JSONValue json)
+	private static CompileCommand fromJson(JSONValue json)
 	{
 		import std.algorithm : map;
 		import std.array : array;
 
 		CompileCommand cc;
 
-		cc.directory = json["directory"].str;
-		cc.file = json["file"].str;
+		cc.directory = enforce("directory" in json, "'directory' missing from Clang compilation database entry")
+			.str;
+		cc.file = enforce("file" in json, "'file' missing from Clang compilation database entry")
+			.str;
 
 		if (auto args = "arguments" in json)
 		{
@@ -146,7 +189,7 @@ private struct CompileCommand
 		else
 		{
 			throw new Exception(
-				"Either 'arguments' or 'command' missing from Clang compilation database");
+				"Either 'arguments' or 'command' missing from Clang compilation database entry");
 		}
 
 		if (auto o = "output" in json)
@@ -168,62 +211,104 @@ private struct CompileCommand
 		return true;
 	}
 
-	void feedOptions(
-		ref HashSet!string imports,
-		ref HashSet!string stringImports,
-		ref HashSet!string fileImports,
-		ref HashSet!string versions,
-		ref HashSet!string debugVersions)
+	bool opCast(T : bool)() const
 	{
-		import std.algorithm : startsWith;
-
-		enum importMark = "-I"; // optional =
-		enum stringImportMark = "-J"; // optional =
-		enum fileImportMark = "-i=";
-		enum versionMark = "-version=";
-		enum debugMark = "-debug=";
-
-		foreach (arg; args)
-		{
-			const mark = arg.startsWith(
-				importMark, stringImportMark, fileImportMark, versionMark, debugMark
-			);
-
-			switch (mark)
-			{
-			case 0:
-				break;
-			case 1:
-			case 2:
-				if (arg.length == 2)
-					break; // ill-formed flag, we don't need to care here
-				const st = arg[2] == '=' ? 3 : 2;
-				const path = getPath(arg[st .. $]);
-				if (mark == 1)
-					imports.put(path);
-				else
-					stringImports.put(path);
-				break;
-			case 3:
-				fileImports.put(getPath(arg[fileImportMark.length .. $]));
-				break;
-			case 4:
-				versions.put(getPath(arg[versionMark.length .. $]));
-				break;
-			case 5:
-				debugVersions.put(getPath(arg[debugMark.length .. $]));
-				break;
-			default:
-				break;
-			}
-		}
+		return isValid;
 	}
 
-	string getPath(string filename)
+	@property CompileCommand dup() const
+	{
+		return CompileCommand(directory, file, args.dup, output);
+	}
+
+	string getNormalizedFilePath() const
+	{
+		return getPath(file).buildNormalizedPath();
+	}
+
+	string getPath(string filename) const
 	{
 		import std.path : absolutePath;
 
 		return absolutePath(filename, directory);
+	}
+
+	Future!(BuildIssue[]) run() const
+	{
+		import std.algorithm : canFind, remove;
+		import std.process : Config, execute;
+
+		return Future!(BuildIssue[]).async({
+			trace("stripping color from ", args);
+			string[] program = args.dup.remove!(a => a.canFind("-color=on") || a.canFind(
+				"-enable-color"));
+			trace("running ", program);
+			auto res = execute(program, null, Config.none, size_t.max, directory);
+			trace(res.status, " ", res.output);
+			auto issues = parseBuildIssues(res.output);
+			trace("found ", issues.length, " issue(s)!");
+			return issues;
+		});
+	}
+}
+
+void feedOptions(
+	in CompileCommand cc,
+	ref HashSet!string imports,
+	ref HashSet!string stringImports,
+	ref HashSet!string fileImports,
+	ref HashSet!string versions,
+	ref HashSet!string debugVersions)
+{
+	import std.algorithm : startsWith;
+
+	enum importMark = "-I"; // optional =
+	enum stringImportMark = "-J"; // optional =
+	enum fileImportMark = "-i=";
+	enum dmdVersionMark = "-version=";
+	enum ldcVersionMark = "--d-version=";
+	enum dmdDebugMark = "-debug=";
+	enum ldcDebugMark = "--d-debug=";
+
+	foreach (arg; cc.args)
+	{
+		const mark = arg.startsWith(
+			importMark, stringImportMark, fileImportMark, dmdVersionMark, ldcVersionMark, dmdDebugMark, ldcDebugMark,
+		);
+
+		switch (mark)
+		{
+		case 0:
+			break;
+		case 1:
+		case 2:
+			if (arg.length == 2)
+				break; // ill-formed flag, we don't need to care here
+			const st = arg[2] == '=' ? 3 : 2;
+			const path = cc.getPath(arg[st .. $]);
+			if (mark == 1)
+				imports.put(path);
+			else
+				stringImports.put(path);
+			break;
+		case 3:
+			fileImports.put(cc.getPath(arg[fileImportMark.length .. $]));
+			break;
+		case 4:
+			versions.put(arg[dmdVersionMark.length .. $]);
+			break;
+		case 5:
+			versions.put(arg[ldcVersionMark.length .. $]);
+			break;
+		case 6:
+			debugVersions.put(arg[dmdDebugMark.length .. $]);
+			break;
+		case 7:
+			debugVersions.put(arg[ldcDebugMark.length .. $]);
+			break;
+		default:
+			break;
+		}
 	}
 }
 
